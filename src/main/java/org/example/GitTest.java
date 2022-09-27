38ebb3fd89e3558681d217330508c8d2818f1590
yqy <547803727@qq.com>
2022-09-27 17:32:07 +0800

    push commit

--- src/main/java/org/example/GitTest.java
+++ src/main/java/org/example/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
         System.out.println("hot-fix Git!");
         System.out.println("hello Git4!");
         System.out.println("master test!");
+        System.out.println("push test!");
     }
 }
